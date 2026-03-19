80b937199261ff736572004f7e8945f5643fef26
parnikagupta14 <gparnika530@gmail.com>
2026-03-19 11:53:04 +0530

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-async function sendMessage(){
-
-let input = document.getElementById("message");
-let chat = document.getElementById("chat");
-
-let msg = input.value;
-
-chat.innerHTML += "<p>You: "+msg+"</p>";
-
-let res = await fetch("http://127.0.0.1:8000/chat",{
-method:"POST",
-headers:{"Content-Type":"application/json"},
-body:JSON.stringify({message:msg})
+function sendMessage(){
+  let input = document.getElementById("message");
+  let chat = document.getElementById("chat");
+  let msg = input.value.trim();
+  if(msg === "") return;
+  chat.innerHTML += "<p><b>You:</b> " + msg + "</p>";
+  fetch("http://127.0.0.1:8000/chat",{
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json"
+  },
+  body:JSON.stringify({message:msg})
+})
+  .then(res => res.json())
+  .then(data => {
+    chat.innerHTML += "<p><b>AI:</b> " + data.response + "</p>";
+    chat.scrollTop = chat.scrollHeight;
+})
+.catch(err => {
+  chat.innerHTML += "<p><b>Error:</b> Server not responding</p>";
 });
-
-let data = await res.json();
-
-chat.innerHTML += "<p>AI: "+data.response+"</p>";
-
 input.value="";
 }
