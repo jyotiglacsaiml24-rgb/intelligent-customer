8763f1fb8c87cfdc75944784ec95518110982f81
parnikagupta14 <gparnika530@gmail.com>
2026-04-15 12:21:35 +0530

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from ai_service import generate_response
 from fastapi.middleware.cors import CORSMiddleware
+from ai_service import generate_response
 from database import save_ticket
 import sqlite3
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -14,15 +13,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 class Query(BaseModel):
     message: str
-
 @app.post("/chat")
 def chat(query: Query):
-
     response, confidence = generate_response(query.message)
-
     save_ticket(query.message, response, confidence)
 
     if confidence < 0.7:
@@ -34,8 +29,10 @@ def chat(query: Query):
 
     return {
         "response": response,
-        "confidence": confidence
+        "confidence": confidence,
+        "escalate": False
     }
+
 
 
 @app.get("/tickets")
@@ -45,9 +42,39 @@ def get_tickets():
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM tickets")
-
     data = cursor.fetchall()
 
     conn.close()
 
     return {"tickets": data}
+
+
+
+@app.post("/feedback")
+def feedback(data: dict):
+
+    query = data.get("query")
+    response = data.get("response")
+
+    conn = sqlite3.connect("support.db")
+    cursor = conn.cursor()
+
+    
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS feedback(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_query TEXT,
+        agent_response TEXT
+    )
+    """)
+
+    
+    cursor.execute(
+        "INSERT INTO feedback (user_query, agent_response) VALUES (?, ?)",
+        (query, response)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return {"message": "Feedback saved successfully"}
